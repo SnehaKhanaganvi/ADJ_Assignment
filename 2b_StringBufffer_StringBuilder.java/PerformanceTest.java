4d6fdc703527d353b7cdb0c03e65a919196f8d58
SnehaKhanaganvi <snehavkhanaganvi123@gmail.com>
2025-05-22 21:56:17 +0530

    Add files via upload

---
+++ 2b_StringBufffer_StringBuilder.java/PerformanceTest.java
@@ -0,0 +1,43 @@
+package string_fucntions;
+
+public class PerformanceTest {
+
+	public static void main(String[] args) {
+		 int iterations = 10000;
+	        String appendString = "AIET";
+
+	        // Test with StringBuffer
+	        StringBuffer stringBuffer = new StringBuffer();
+	        long startTimeBuffer = System.nanoTime();
+
+	        for (int i = 0; i < iterations; i++) {
+	            stringBuffer.append(appendString);
+	        }
+
+	        long endTimeBuffer = System.nanoTime();
+	        long durationBuffer = endTimeBuffer - startTimeBuffer;
+
+	        // Test with StringBuilder
+	        StringBuilder stringBuilder = new StringBuilder();
+	        long startTimeBuilder = System.nanoTime();
+
+	        for (int i = 0; i < iterations; i++) {
+	            stringBuilder.append(appendString);
+	        }
+
+	        long endTimeBuilder = System.nanoTime();
+	        long durationBuilder = endTimeBuilder - startTimeBuilder;
+
+	        // Output results
+	        System.out.println("Time taken by StringBuffer: " + durationBuffer + " nanoseconds");
+	        System.out.println("Time taken by StringBuilder: " + durationBuilder + " nanoseconds");
+
+	        // Justification
+	        if (durationBuilder < durationBuffer) {
+	            System.out.println("\nStringBuilder is faster than StringBuffer for appending strings.");
+	        } else {
+	            System.out.println("\nStringBuffer is faster or equal in speed compared to StringBuilder.");
+	        }
+	}
+
+}
