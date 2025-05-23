0383e203ffc56fb182ca5b96c8e7518fa9d9893e
SnehaKhanaganvi <snehavkhanaganvi123@gmail.com>
2025-05-23 17:48:44 +0530

    ColorDelete.java

--- 1b_ArrayLIst_Exercise.java/ColorDelete.java
+++ 1b_ArrayLIst_Exercise.java/ColorDelete.java
@@ -1,3 +1,5 @@
+6. Write a java program for getting different colors through ArrayList interface and delete nth  element from the ArrayList object by using remove by index 
+
 package arraylistinterface;
 
 import java.util.ArrayList;
